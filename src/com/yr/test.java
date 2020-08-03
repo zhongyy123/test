34f29e38d5fbe41dad8513420f17619fda1b7a37
zhoongyy123 <1298661952@qq.com>
2020-08-03 19:23:51 +0800

    更新项目了2

--- src/com/yr/test.java
+++ src/com/yr/test.java
@@ -5,12 +5,18 @@ public class test {
     public static void main(String[] args) {
         test1();
         test2();
+        test3();
     }
 
     static void test1() {
         System.out.println("测试111");
     }
+
     static void test2() {
-        System.out.println("测试idea");
+        System.out.println("测试222");
+    }
+
+    static void test3() {
+        System.out.println("测试333");
     }
 }
