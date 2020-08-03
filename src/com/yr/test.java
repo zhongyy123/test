20e900237f445abb3d995a3ac03266579696126c
zhongyy123 <68989117+zhongyy123@users.noreply.github.com>
2020-08-03 16:49:48 +0800

    Update test.java

--- src/com/yr/test.java
+++ src/com/yr/test.java
@@ -8,7 +8,7 @@ public class test {
     }
 
     static void test1() {
-        System.out.println("测试");
+        System.out.println("测试111");
     }
     static void test2() {
         System.out.println("测试idea");
