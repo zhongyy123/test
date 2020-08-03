63fe06f40f5b8d76ff9b1ecd35483e8e1309e25c
zhongyy123 <68989117+zhongyy123@users.noreply.github.com>
2020-08-03 16:45:52 +0800

    更新
    更新数据

--- src/com/yr/test.java
+++ src/com/yr/test.java
@@ -4,10 +4,13 @@ public class test {
 
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     static void test1() {
         System.out.println("测试");
     }
-
+    static void test2() {
+        System.out.println("测试idea");
+    }
 }
