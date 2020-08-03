83c2823d58d4c31a9ce0db1ccc21b4ab09c98261
zhoongyy123 <1298661952@qq.com>
2020-08-03 16:42:51 +0800

    使用idea提交项目

--- src/com/yr/test.java
+++ src/com/yr/test.java
@@ -2,7 +2,12 @@ package com.yr;
 
 public class test {
 
-    public void test() {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    static void test1() {
         System.out.println("测试");
     }
+
 }
